663991087b211140c301e426c12c7cfb8bf99b5d
Denis <balaev@mail.ru>
2023-11-02 21:01:44 +0300

    2 c FusedLocationProviderClient

--- app/src/main/java/com/example/powerapplication2/MainActivity.kt
+++ app/src/main/java/com/example/powerapplication2/MainActivity.kt
@@ -1,34 +1,201 @@
 package com.example.powerapplication2
 
-import android.content.Intent
-import android.net.Uri
-import android.os.Build
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.provider.Settings
-import android.util.Log
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.powerapplication2.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+
+open class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     val binding by viewBinding(ActivityMainBinding::bind)
-
-    private val requestOverlayPermission = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) {}
+    private val TAG = "LocationActivity"
+    var fusedClient: FusedLocationProviderClient? = null
+    private var mRequest: LocationRequest? = null
+    private var mCallback: LocationCallback? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
-            val intent = Intent(
-                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                Uri.parse("package:$packageName")
-            )
-            requestOverlayPermission.launch(intent)
+        mCallback = object : LocationCallback() {
+            //This callback is where we get "streaming" location updates. We can check things like accuracy to determine whether
+            //this latest update should replace our previous estimate.
+            override fun onLocationResult(locationResult: LocationResult?) {
+                if (locationResult == null) {
+                    Log.d(TAG, "locationResult null")
+                    return
+                }
+                Log.d(
+                    TAG,
+                    "received " + locationResult.getLocations().size.toString() + " locations"
+                )
+                for (loc in locationResult.getLocations()) {
+                    binding.locUpdate.append(
+                        """
+                                                        
+                                                        ${loc.provider}:Accu:(${loc.accuracy}). Lat:${loc.latitude},Lon:${loc.longitude}
+                                                        """.trimIndent()
+                    )
+                }
+            }
+
+            override fun onLocationAvailability(locationAvailability: LocationAvailability) {
+                Log.d(TAG, "locationAvailability is " + locationAvailability.isLocationAvailable)
+                super.onLocationAvailability(locationAvailability)
+            }
+        }
+
+        //permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            //request permission.
+            //However check if we need to show an explanatory UI first
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            ) {
+                showRationale()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_NETWORK_STATE
+                    ), 2
+                )
+            }
+        } else {
+            //we already have the permission. Do any location wizardry now
+            locationWizardry()
         }
     }
+
+    @SuppressLint("MissingPermission")
+    private fun locationWizardry() {
+        fusedClient = LocationServices.getFusedLocationProviderClient(this)
+        //Initially, get last known location. We can refine this estimate later
+        fusedClient!!.lastLocation.addOnSuccessListener { location ->
+            if (location != null) {
+                val loc =
+                    location.provider + ":Accu:(" + location.accuracy + "). Lat:" + location.latitude + ",Lon:" + location.longitude
+                binding.lastLocation.text = loc
+            }
+        }
+
+
+        //now for receiving constant location updates:
+        createLocRequest()
+        val builder: LocationSettingsRequest.Builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(mRequest!!)
+
+        //This checks whether the GPS mode (high accuracy,battery saving, device only) is set appropriately for "mRequest". If the current settings cannot fulfil
+        //mRequest(the Google Fused Location Provider determines these automatically), then we listen for failutes and show a dialog box for the user to easily
+        //change these settings.
+        val client: SettingsClient = LocationServices.getSettingsClient(this@MainActivity)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+        task.addOnFailureListener { e ->
+            if (e is ResolvableApiException) {
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    val resolvable: ResolvableApiException = e as ResolvableApiException
+                    resolvable.startResolutionForResult(this@MainActivity, 500)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+
+        //actually start listening for updates: See on Resume(). It's done there so that conveniently we can stop listening in onPause
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        fusedClient?.removeLocationUpdates(mCallback)
+    }
+
+    @SuppressLint("MissingPermission")
+    protected fun startLocationUpdates() {
+        fusedClient?.requestLocationUpdates(mRequest, mCallback, null)
+    }
+
+
+    private fun createLocRequest() {
+        mRequest = LocationRequest()
+        mRequest!!.interval = 10000 //time in ms; every ~10 seconds
+        mRequest!!.fastestInterval = 5000
+        mRequest!!.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            2 -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Thanks bud", Toast.LENGTH_SHORT).show()
+                    locationWizardry()
+                } else {
+                    Toast.makeText(this, "C'mon man we really need this", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {}
+        }
+    }
+
+
+    private fun showRationale() {
+        val dialog: AlertDialog = AlertDialog.Builder(this).setMessage(
+            "We need this, Just suck it up and grant us the" +
+                    "permission :)"
+        ).setPositiveButton("Sure") { dialogInterface, i ->
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                2
+            )
+            dialogInterface.dismiss()
+        }
+            .create()
+        dialog.show()
+    }
+
 }
