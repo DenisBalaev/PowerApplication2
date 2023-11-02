3efe94a3475cbf1b1a8a4aceed9ff2ae206d7278
Denis <balaev@mail.ru>
2023-11-02 20:47:23 +0300

    1

--- app/src/main/java/com/example/powerapplication2/MainActivity.kt
+++ app/src/main/java/com/example/powerapplication2/MainActivity.kt
@@ -7,24 +7,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.powerapplication2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     val binding by viewBinding(ActivityMainBinding::bind)
-    val REQUEST_OVERLAY_PERMISSION = 1
+
+    private val requestOverlayPermission = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("tagPower","MainActivity")
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && !Settings.canDrawOverlays(this)) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
             val intent = Intent(
                 Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
                 Uri.parse("package:$packageName")
             )
-            startActivityForResult(intent, REQUEST_OVERLAY_PERMISSION)
+            requestOverlayPermission.launch(intent)
         }
     }
 }
